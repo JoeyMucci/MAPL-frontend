1bfd8b7d3ec01b55236d04b3f6c5e4e4316b5ab6
JoeyMucci <jmucci314@gmail.com>
2025-07-05 08:35:13 -0400

    rivalry page finished

--- src/app/rivalry/page.tsx
+++ src/app/rivalry/page.tsx
@@ -7,9 +7,8 @@ import { RivalryHeader } from "@/components/Headers/RivalryHeader";
 import { Bout } from "@/components/Bout/Bout";
 import { SimpleBout } from "@/types/bouts";
 import { RivalryPebbles, RivalryResults } from "@/types/stats";
-import { divisions } from "@/vars";
-import axios from "axios";
 import { pebblerNameList } from "@/vars";
+import axios from "axios";
 
 export default function RivalryPage() {
     let largeScreen = useMediaQuery('(min-width: 56em)')
@@ -26,78 +25,73 @@ export default function RivalryPage() {
         }
     }
 
-    const [pebblerOne, setPebblerOne] = useState<string>("Cream");
-    const [pebblerTwo, setPebblerTwo] = useState<string>("Flapper")
-    const [pebbleBreakdown, setPebbleBreakdown] = useState<{ [division: string]: RivalryPebbles }>()
+    useEffect(() => {
+        let firstRandom = Math.floor(Math.random() * pebblerNameList.length)
+        let secondRandom = firstRandom
+
+        while (firstRandom == secondRandom) {
+            secondRandom = Math.floor(Math.random() * pebblerNameList.length)
+        }
+        setPebblerOne(pebblerNameList[firstRandom])
+        setPebblerTwo(pebblerNameList[secondRandom])
+    }, [])
+
+    const [pebblerOne, setPebblerOne] = useState<string>("");
+    const [pebblerTwo, setPebblerTwo] = useState<string>("")
+    const [pebbleBreakdown, setPebbleBreakdown] = useState<{ [division: string]: RivalryPebbles }>({})
     const [resultBreakdown, setResultBreakdown] = useState<{ [division: string]: RivalryResults }>({})
     const [bouts, setBouts] = useState<SimpleBout[]>([])
 
-    let oneScore = 0
-    let twoScore = 0
-    let oneWins = 0
-    let twoWins = 0
-    let ties = 0
-
-    const dataFetched = (
-        pebbleBreakdown &&
-        Object.keys(pebbleBreakdown).length > 0 &&
-        resultBreakdown &&
-        Object.keys(resultBreakdown).length > 0
-    )
-
-    if (dataFetched)
-        divisions.forEach((division) => {
-            oneScore += pebbleBreakdown[division]?.one_score || 0
-            twoScore += pebbleBreakdown[division]?.two_score || 0
-            oneWins += resultBreakdown[division]?.one_wins || 0
-            twoWins += resultBreakdown[division]?.two_wins || 0
-            ties += resultBreakdown[division]?.ties || 0
-        })
-
     useEffect(() => {
-        fetchRivalry(pebblerOne, pebblerTwo).then((data) => {
-            console.log(data)
-            setPebbleBreakdown(data.division_pebbles)
-            setResultBreakdown(data.division_wtl)
-            setBouts(data.bouts)
-        });
+        if (pebblerOne.length > 0) {
+            fetchRivalry(pebblerOne, pebblerTwo).then((data) => {
+                setPebbleBreakdown(data.division_pebbles)
+                setResultBreakdown(data.division_wtl)
+                setBouts(data.bouts)
+            })
+        }
     }, [pebblerOne, pebblerTwo]);
 
     return (
         <>
-            <RivalryHeader
-                pebblerOne={pebblerOne}
-                pebblerTwo={pebblerTwo}
-                rp={{ "one_score": oneScore, "two_score": twoScore }}
-                rr={{ "one_wins": oneWins, "two_wins": twoWins, "ties": ties }}
-                toggleOneAction={setPebblerOne}
-                toggleTwoAction={setPebblerTwo}
-            />
+            {pebblerOne.length > 0 &&
+                <>
+                    <RivalryHeader
+                        pebblerOne={pebblerOne}
+                        pebblerTwo={pebblerTwo}
+                        rp={pebbleBreakdown}
+                        rr={resultBreakdown}
+                        toggleOneAction={setPebblerOne}
+                        toggleTwoAction={setPebblerTwo}
+                        largeScreen={largeScreen}
+                    />
 
-            <Stack align="center" mt="md" mb="md">
-                {!dataFetched ? (
-                    <div>loading</div>
-                ) : (
-                    bouts.length === 0 ? (
-                        <div>No bouts found.</div>
-                    ) :
-
-                        largeScreen ? (
-                            Array.from({ length: Math.ceil(bouts.length / 3) }).map((_, rowIdx) => (
-                                <Flex key={rowIdx} gap="md">
-                                    {bouts.slice(rowIdx * 3, rowIdx * 3 + 3).map((bout, colIdx) => (
-                                        <Bout key={rowIdx * 3 + colIdx} bout={bout} showDate />
-                                    ))}
-                                </Flex>
-                            ))
+                    <Stack align="center" mt="md" mb="md">
+                        {Object.keys(pebbleBreakdown).length == 0 ? (
+                            <div>loading</div>
                         ) : (
-                            bouts.map((bout, i) => (
-                                <Bout key={i} bout={bout} showDate />
-                            ))
-                        )
+                            bouts.length === 0 ? (
+                                <div>No bouts found.</div>
+                            ) :
 
-                )}
-            </Stack>
+                                largeScreen ? (
+                                    Array.from({ length: Math.ceil(bouts.length / 3) }).map((_, rowIdx) => (
+                                        <Flex key={rowIdx} gap="md">
+                                            {bouts.slice(rowIdx * 3, rowIdx * 3 + 3).map((bout, colIdx) => (
+                                                <Bout key={rowIdx * 3 + colIdx} bout={bout} showDate />
+                                            ))}
+                                        </Flex>
+                                    ))
+                                ) : (
+                                    bouts.map((bout, i) => (
+                                        <Bout key={i} bout={bout} showDate />
+                                    ))
+                                )
+
+                        )}
+                    </Stack>
+                </>
+            }
         </>
     )
 }
--- src/components/Headers/RivalryHeader.tsx
+++ src/components/Headers/RivalryHeader.tsx
@@ -1,20 +1,22 @@
 "use client"
 
 import { FC } from "react";
-import { Flex, Container, Stack, Image, Title } from "@mantine/core";
+import { Badge, Flex, Container, Stack, Image, Title, Text, rem } from "@mantine/core";
 import { DonutChart } from "@mantine/charts";
 import { SearchableSelect } from "./SearchableSelect";
 import { toCamelCase } from "@/functions"
-import classes from "./Header.module.css";
 import { RivalryPebbles, RivalryResults } from "@/types/stats";
+import { divisions, colorMap } from "@/vars";
+import classes from "./Header.module.css";
 
 export const RivalryHeader: FC<{
     pebblerOne: string,
     pebblerTwo: string,
-    rp: RivalryPebbles,
-    rr: RivalryResults,
+    rp: { [division: string]: RivalryPebbles },
+    rr: { [division: string]: RivalryResults },
     toggleOneAction: (a: string) => void,
     toggleTwoAction: (a: string) => void,
+    largeScreen: boolean,
 }> = (
     {
         pebblerOne,
@@ -23,91 +25,173 @@ export const RivalryHeader: FC<{
         rr,
         toggleOneAction,
         toggleTwoAction,
+        largeScreen,
     }) => {
+        // Totals across the divisions
+        let one_score = 0
+        let two_score = 0
+        let one_wins = 0
+        let two_wins = 0
+        let ties = 0
+        const validDivisions: string[] = []
+
+        divisions.forEach((division, _) => {
+            one_score += rp[division]?.one_score
+            two_score += rp[division]?.two_score
+            one_wins += rr[division]?.one_wins
+            two_wins += rr[division]?.two_wins
+            ties += rr[division]?.ties
+            if (rp[division]?.one_score + rp[division]?.two_score > 0) {
+                validDivisions.push(division)
+            }
+        })
+
+
+        const MiddleBlock = () => (
+            <Stack align="center" gap="xl">
+                <Stack align="center">
+                    <Title ta="center" order={1}>Pebble Breakdown</Title>
+                    <Flex gap="md">
+                        <Title order={3} c="goodGreen">{one_score}</Title>
+                        <Title order={3}>-</Title>
+                        <Title order={3} c="alarmRed">{two_score}</Title>
+                    </Flex>
+                    <Stack h={75}>
+                        <DonutChart
+                            startAngle={180}
+                            endAngle={0}
+                            size={150}
+                            strokeWidth={0}
+                            thickness={25}
+                            withTooltip={false}
+                            data={[
+                                { name: pebblerOne, value: one_score, color: "goodGreen" },
+                                { name: pebblerTwo, value: two_score, color: "alarmRed" },
+                            ]}
+                        />
+                    </Stack>
+
+                    {largeScreen ? (
+                        Array.from({ length: Math.ceil(validDivisions.length / 2) }).map((_, rowIdx) => (
+                            <Flex key={rowIdx} gap="md">
+                                {validDivisions.slice(rowIdx * 2, rowIdx * 2 + 2).map((division, colIdx) => (
+                                    <Stack key={colIdx} align="center" gap={rem(0)}>
+                                        <Badge w={125} color={colorMap[division]}>{division}</Badge>
+                                        <Flex gap="md">
+                                            <Title order={6} c="goodGreen">{rp[division].one_score}</Title>
+                                            <Title order={6}>-</Title>
+                                            <Title order={6} c="alarmRed">{rp[division].two_score}</Title>
+                                        </Flex>
+                                    </Stack>
+                                ))}
+                            </Flex>
+                        ))
+                    ) : (
+                        validDivisions.map((division, i) => (
+                            <Stack key={i} align="center" gap={rem(0)}>
+                                <Badge w={125} color={colorMap[division]}>{division}</Badge>
+                                <Flex gap="md">
+                                    <Title order={6} c="goodGreen">{rp[division].one_score}</Title>
+                                    <Title order={6}>-</Title>
+                                    <Title order={6} c="alarmRed">{rp[division].two_score}</Title>
+                                </Flex>
+                            </Stack>
+                        ))
+                    )}
+                </Stack>
+
+                <Stack align="center">
+                    <Title ta="center" order={1}>Result Breakdown</Title>
+                    <Flex gap="md">
+                        <Title order={3} c="goodGreen">{one_wins}</Title>
+                        <Title order={3}>-</Title>
+                        <Title order={3} c="midBlue">{ties}</Title>
+                        <Title order={3}>-</Title>
+                        <Title order={3} c="alarmRed">{two_wins}</Title>
+                    </Flex>
+                    <Stack h={75}>
+                        <DonutChart
+                            startAngle={180}
+                            endAngle={0}
+                            size={150}
+                            strokeWidth={0}
+                            thickness={25}
+                            withTooltip={false}
+                            data={[
+                                { name: pebblerOne, value: one_wins, color: "goodGreen" },
+                                { name: "ties", value: ties, color: "midBlue" },
+                                { name: pebblerTwo, value: two_wins, color: "alarmRed" },
+                            ]}
+                        />
+                    </Stack>
+                    {largeScreen ? (
+                        Array.from({ length: Math.ceil(validDivisions.length / 2) }).map((_, rowIdx) => (
+                            <Flex key={rowIdx} gap="md">
+                                {validDivisions.slice(rowIdx * 2, rowIdx * 2 + 2).map((division, colIdx) => (
+                                    <Stack key={colIdx} align="center" gap={rem(0)}>
+                                        <Badge w={125} color={colorMap[division]}>{division}</Badge>
+                                        <Flex gap="md">
+                                            <Title order={6} c="goodGreen">{rr[division].one_wins}</Title>
+                                            <Title order={6}>-</Title>
+                                            <Title order={6} c="midBlue">{rr[division].ties}</Title>
+                                            <Title order={6}>-</Title>
+                                            <Title order={6} c="alarmRed">{rr[division].two_wins}</Title>
+                                        </Flex>
+                                    </Stack>
+                                ))}
+                            </Flex>
+                        ))
+                    ) : (
+                        validDivisions.map((division, i) => (
+                            <Stack key={i} align="center" gap={rem(0)}>
+                                <Badge w={125} color={colorMap[division]}>{division}</Badge>
+                                <Flex gap="md">
+                                    <Title order={6} c="goodGreen">{rr[division].one_wins}</Title>
+                                    <Title order={6}>-</Title>
+                                    <Title order={6} c="midBlue">{rr[division].ties}</Title>
+                                    <Title order={6}>-</Title>
+                                    <Title order={6} c="alarmRed">{rr[division].two_wins}</Title>
+                                </Flex>
+                            </Stack>
+                        ))
+                    )}
+                </Stack >
+            </Stack >
+        )
+
+        const SearchBlock: FC<{ pebbler: string, opp: string, left: boolean }> = ({ pebbler, opp, left }) => (
+            <Stack>
+                <Image
+                    src={"/pebblers/" + toCamelCase(pebbler) + ".png"}
+                    alt={"Image of " + toCamelCase(pebbler) + " the pebbler"}
+                    width={300}
+                    height={300}
+                    className={left ? "" : classes.flipY}
+                />
+                <SearchableSelect
+                    defaultValue={pebbler}
+                    disabledName={opp}
+                    setParentAction={left ? toggleOneAction : toggleTwoAction}
+                    flip={!left}
+                />
+            </Stack>
+        )
+
         return (
-            <Container fluid className={classes.header} pt="md">
-                <Flex justify="space-evenly" align="center">
-                    <Stack>
-                        <Image
-                            src={"/pebblers/" + toCamelCase(pebblerOne) + ".png"}
-                            alt={"Image of " + toCamelCase(pebblerOne) + " the pebbler"}
-                            width={300}
-                            height={300}
-                        />
-                        <SearchableSelect
-                            defaultValue={pebblerOne}
-                            disabledName={pebblerTwo}
-                            setParentAction={toggleOneAction}
-                        />
-                    </Stack>
+            <Container fluid className={classes.header} pt="md" pb="md">
+                {largeScreen ? (
+                    <Flex justify="space-evenly" align="center">
+                        <SearchBlock pebbler={pebblerOne} opp={pebblerTwo} left={true} />
+                        {one_score + two_score > 0 && <MiddleBlock />}
+                        <SearchBlock pebbler={pebblerTwo} opp={pebblerOne} left={false} />
+                    </Flex>
+                ) : (
                     <Stack align="center">
-                        <Stack align="center">
-                            <Title order={1}>Pebble Breakdown</Title>
-                            <Flex gap="md">
-                                <Title order={3} c="goodGreen">{rp.one_score}</Title>
-                                <Title order={3}>-</Title>
-                                <Title order={3} c="alarmRed">{rp.two_score}</Title>
-                            </Flex>
-                            <Stack h={75}>
-                                <DonutChart
-                                    startAngle={180}
-                                    endAngle={0}
-                                    size={150}
-                                    strokeWidth={0}
-                                    thickness={25}
-                                    withTooltip={false}
-                                    data={[
-                                        { name: pebblerOne, value: rp.one_score, color: "goodGreen" },
-                                        { name: pebblerTwo, value: rp.two_score, color: "alarmRed" },
-                                    ]}
-                                />
-                            </Stack>
-                        </Stack>
-
-
-                        <Stack align="center" mb="xl">
-                            <Title order={1}>Result Breakdown</Title>
-                            <Flex gap="md">
-                                <Title order={3} c="goodGreen">{rr.one_wins}</Title>
-                                <Title order={3}>-</Title>
-                                <Title order={3} c="midBlue">{rr.ties}</Title>
-                                <Title order={3}>-</Title>
-                                <Title order={3} c="alarmRed">{rr.two_wins}</Title>
-                            </Flex>
-                            <Stack h={75}>
-                                <DonutChart
-                                    startAngle={180}
-                                    endAngle={0}
-                                    size={150}
-                                    strokeWidth={0}
-                                    thickness={25}
-                                    withTooltip={false}
-                                    data={[
-                                        { name: pebblerOne, value: rr.one_wins, color: "goodGreen" },
-                                        { name: "ties", value: rr.ties, color: "midBlue" },
-                                        { name: pebblerTwo, value: rr.two_wins, color: "alarmRed" },
-                                    ]}
-                                />
-                            </Stack>
-                        </Stack>
+                        <SearchBlock pebbler={pebblerOne} opp={pebblerTwo} left={true} />
+                        {one_score + two_score > 0 && <MiddleBlock />}
+                        <SearchBlock pebbler={pebblerTwo} opp={pebblerOne} left={false} />
                     </Stack>
-
-                    <Stack>
-                        <Image
-                            src={"/pebblers/" + toCamelCase(pebblerTwo) + ".png"}
-                            alt={"Image of " + toCamelCase(pebblerTwo) + " the pebbler"}
-                            width={300}
-                            height={300}
-                            className={classes.flipY}
-                        />
-                        <SearchableSelect
-                            defaultValue={pebblerTwo}
-                            disabledName={pebblerOne}
-                            setParentAction={toggleTwoAction}
-                            flip
-                        />
-                    </Stack>
-                </Flex>
+                )}
             </Container>
         )
     }
