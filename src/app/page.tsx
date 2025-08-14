0db13ea03b378fa888af58be456abf0ad732f57a
JoeyMucci <jmucci314@gmail.com>
2025-08-14 13:55:58 -0400

    home hero header with placeholder background image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+"use client"
+
+import { HomeHeader } from "@/components/Headers/HomeHeader"
+import { useMediaQuery } from "@mantine/hooks"
+
 export default function HomePage() {
+    let largeScreen = useMediaQuery('(min-width: 56em)')
+    largeScreen = largeScreen === undefined ? true : largeScreen
+
     return (
-        <main>
-            <h1>HOME</h1>
-            <p>This is a placeholder page for HOME.</p>
-        </main>
-    );
+        <HomeHeader largeScreen={largeScreen} />
+    )
 }
