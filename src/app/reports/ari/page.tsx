2d2fe2bd53c52b9c21ef22e63ded714b11a38949
Joey Mucci <jmucci314@gmail.com>
2025-12-31 22:31:43 -0500

    make reporter fetching dynamic

--- src/app/reports/ari/page.tsx
+++ src/app/reports/ari/page.tsx
@@ -8,6 +8,8 @@ export const metadata: Metadata = {
   title: 'MAPL | Reports'
 };
 
+export const dynamic = "force-dynamic";
+
 export default async function ReporterPage() {
     async function fetchReporter(authorName: string) {
         try {
